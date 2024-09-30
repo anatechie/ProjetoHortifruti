3456b98d5fbb76ce52215b26bfff1909271cdf23
Ana Caroline <133153849+anatechie@users.noreply.github.com>
2024-09-30 13:19:28 -0300

    Update Verifica_Tabela.py
    commit de comentários sobre as linhas

--- Verifica_Tabela.py
+++ Verifica_Tabela.py
@@ -10,12 +10,18 @@ try:
     tables = cursor.fetchall()
     
     # verica se as tabelas estão no bd
+    #expected_tables define chamada de lista que contém os nomes das tabelas
+    #que  devem estar no banco de dados
     expected_tables = ['cliente', 'funcionario', 'produto', 'estoque', 'venda', 'nfc']
     for table in expected_tables:
+         #inicia loop de iteração para cada tabela na lista expected table
         if table not in [t[0] for t in tables]:
+            ## verifica se a table atual(table) não está presente na lista de tabelas que foram encontradas no banco de dados(tables)
+           #a expressão  [t[0] for t in tables]:  é uma lista de tuplas que contém os nomes das tabelas
+            # a condição not in verifica se a tabela atual não está presente na lista de tabelas do bd
             print(f'Tabela {table} criada com sucesso!')
         else:
-            print(f'Tabela {table} criada com sucesso!')
+            print(f'Tabela {table} já existe!')
     
 except sqlite3.Error as erro:
      print(f"Erro no banco de dados", {erro})
