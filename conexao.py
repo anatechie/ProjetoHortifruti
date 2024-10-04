27b9ef4749fc3e34304752864663102d748fbf9a
Ana Caroline <133153849+anatechie@users.noreply.github.com>
2024-10-03 22:00:29 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -1,63 +1,34 @@
-import sqlite3 as con
+import sqlite3 
 
-insere_cliente = '''
-INSERT INTO cliente(Nome, Sobrenome, RG, CPF, Telefone, Endereco, Data_Nascimento, Email, id_funcionario)
-VALUES ("Ana", "Rodrigues", "12345678", "12345678910", "Rua 7", "5561999999999", "2024-12-08", "ana@hotmail.com", 1);
-'''
 
-insere_produto = '''
-INSERT INTO cliente(Nome, Preco, Tipo_Produto, Peso, Quantidade, Descricao)
-VALUES ("Manga", "5,37",  "Fruta", "0,5", "10", "Manga verde");
+con = sqlite3.connect('hortifruti.db')
+     
+try:
+    #verifica se as tabelas foram criadas
+    cursor= con.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    
+    # verica se as tabelas estão no bd
+    #expected_tables define chamada de lista que contém os nomes das tabelas
+    #que  devem estar no banco de dados
+    expected_tables = ['cliente', 'funcionario', 'fornecedor', 'produto', 'estoque', 'venda', 'nfc']
+    for table in expected_tables:
+         #inicia loop de iteração para cada tabela na lista expected table
+        if table not in [t[0] for t in tables]:
+            ## verifica se a table atual(table) não está presente na lista de tabelas que foram encontradas no banco de dados(tables)
+           #a expressão  [t[0] for t in tables]:  é uma lista de tuplas que contém os nomes das tabelas
+            # a condição not in verifica se a tabela atual não está presente na lista de tabelas do bd
+            print(f'Tabela {table} criada com sucesso!')
+        else:
+            print(f'Tabela {table} já existe!')
+    
+except sqlite3.Error as erro:
+     print(f"Erro no banco de dados", {erro})
 
-'''
-
-insere_venda = '''
-INSERT INTO cliente(Quantidade, Preco, Nota_Fiscal)
-VALUES ("1", "5,37",  "123456");
-
-'''
-
-insere_funcionario = '''
-INSERT INTO cliente(Nome, Sobrenome, CPF, Endereço, Telefone, Cargo, Status)
-VALUES ("Breno", "Ferreira", "12333466666", "Quadra 314", "98342384",  "Caixa", "Ativo");
-
-'''
-
-insere_fornecedor = '''
-INSERT INTO cliente(CNPJ, Telefone, Endereco, Email, Nome)
-VALUES ("22455434", "40028922", "Rua paraiso", "adaoeva@gmail.com", "Adão e Eva Hortifrutigranjeiro");
-'''
-
-insere_estoque= '''
-INSERT INTO cliente(Qntd_Produto, Preco_Compra, Preco_Venda, Tipo_Produto)
-VALUES ("12", "64,44",  "5,37", "Fruta");
-
-'''
-
-insere_nfc = '''
-INSERT INTO cliente(id_NFC, id_Cliente, id_Produto, Data_Compra)
-VALUES ("54", "633", "21", "2024-10-03");
-'''
-
-try: 
-    conexao = con.connect('hortifruti.db')
-    cursor = conexao.cursor()
-
-    cursor.execute(insere_cliente)
-    cursor.execute(insere_produto)
-    cursor.execute(insere_venda)
-    cursor.execute(insere_funcionario)
-    cursor.execute(insere_fornecedor)
-    cursor.execute(insere_estoque)
-    cursor.execute(insere_nfc)
-
-    conexao.commit()
-    print('Dados inseridos com sucesso')
-except con.Error as erro:
-    print(f'Erro ao inserir dados: {erro}')
-else:
-    res =cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    print(res.fetchall())
 finally:
-    if conexao:
-        conexao.close()
+    if con:
+     con.close()
+
+#Sqlite_master é uma tabela mestra do sqlite contendo informações das tabelas dos bancos de dados
+
